08e4a699c47b76d4158b7c5837e651bd80413909
chkim116 <dudnqnfqlc@naver.com>
2022-04-30 14:06:29 +0900

    Initial Commit

---
+++ compile/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  compile
+//
+//  Created by 김창회 on 2022/04/30.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
